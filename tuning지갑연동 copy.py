8f033dd6b0d9f21cc05b187749ecfae8373357b8
You726 <cksdid726@naver.com>
2022-06-27 16:25:23 +0900

    d
    d

--- tuning지갑연동 copy.py
+++ tuning지갑연동 copy.py
@@ -1,56 +1,4 @@
-
 import os
-import pyupbit
-
-from flask import Flask, jsonify
-from flask import request
-
-app1 = Flask(__name__)
-strs = []
-currencies = []
-
-access_key = "KlxjvWx33PG1cSY4BIf8fRW9WbpACAa03bS95qZg";
-secret_key = "nYQ6L4o6Bz2Jl09kDGy6FUmoBstHXS7WaGuwx0T5";
-server_url = "https://api.upbit.com";
-
-@app1.route('/get', methods = ['GET', 'POST'])
-def a():
-    return jsonify({
-    'uid' : "",
-    'currencies' : currencies,
-    'amount' : strs})
-
-@app1.route('/add', methods = ['GET'])
-def b():
-    get_currencies()
-    
-@app1.route('/read', methods=['POST'])
-def c():
-    access_key = request.form['accessKey']
-    secret_key = request.form['secretKey']
-    print(access_key + secret_key)
-    return access_key + secret_key
-
-upbit = pyupbit.Upbit(access_key, secret_key)
-
-balances = upbit.get_balances()
-
-def get_currencies():  
-    for i in balances:
-        amount = upbit.get_amount(i['currency']);
-        if(amount > 0):
-            currencies.append(i['currency'])
-            strs.append(amount)
-
-def get_currency_amount(cname):  
-    am = 0;
-    for i in balances:
-        if(i['currency'] == cname):
-            upbit.get_amount(i['currency']);
-    return am;
-
-if __name__ == '__main__':
-    app1.run(host='0.0.0.0', port=5000, debug=True)import os
 import pyupbit
 
 from flask import Flask, jsonify
@@ -126,5 +74,5 @@ def get_currency_amount(cname):
 
 
 if __name__ == '__main__':
-    app1.run(host='0.0.0.0', port=5000, debug=True)
+    app1.run(host='127.0.0.1', port=5000, debug=True)
     

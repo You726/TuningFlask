72bbe432b3563153a2a829784559a1a326703286
You726 <cksdid726@naver.com>
2022-06-28 15:00:45 +0900

    Update tuning지갑연동.py

--- tuning지갑연동.py
+++ tuning지갑연동.py
@@ -1,56 +1,4 @@
-
 import os
-import pyupbit
-
-from flask import Flask, jsonify
-from flask import request
-
-app1 = Flask(__name__)
-strs = []
-currencies = []
-
-access_key = "KlxjvWx33PG1cSY4BIf8fRW9WbpACAa03bS95qZg";
-secret_key = "nYQ6L4o6Bz2Jl09kDGy6FUmoBstHXS7WaGuwx0T5";
-server_url = "https://api.upbit.com";
-
-@app1.route('/get', methods = ['GET', 'POST'])
-def a():
-    return jsonify({
-    'uid' : "",
-    'currencies' : currencies,
-    'amount' : strs})
-
-@app1.route('/add', methods = ['GET'])
-def b():
-    get_currencies()
-    
-@app1.route('/read', methods=['POST'])
-def c():
-    access_key = request.form['accessKey']
-    secret_key = request.form['secretKey']
-    print(access_key + secret_key)
-    return access_key + secret_key
-
-upbit = pyupbit.Upbit(access_key, secret_key)
-
-balances = upbit.get_balances()
-
-def get_currencies():  
-    for i in balances:
-        amount = upbit.get_amount(i['currency']);
-        if(amount > 0):
-            currencies.append(i['currency'])
-            strs.append(amount)
-
-def get_currency_amount(cname):  
-    am = 0;
-    for i in balances:
-        if(i['currency'] == cname):
-            upbit.get_amount(i['currency']);
-    return am;
-
-if __name__ == '__main__':
-    app1.run(host='0.0.0.0', port=5000, debug=True)import os
 import pyupbit
 
 from flask import Flask, jsonify
@@ -66,17 +14,15 @@ strs = []
 currencies = []
 puid = "";
 
-access_key = "KlxjvWx33PG1cSY4BIf8fRW9WbpACAa03bS95qZg";
-secret_key = "nYQ6L4o6Bz2Jl09kDGy6FUmoBstHXS7WaGuwx0T5";
+access_key = "alxjvWx33PG1cSY4BIf8fRW9WbpACAa03bS95qZg";
+secret_key = "aYQ6L4o6Bz2Jl09kDGy6FUmoBstHXS7WaGuwx0T5";
 server_url = "https://api.upbit.com";
 
 project_url = "https://console.firebase.google.com/project/tuning-9b90e/firestore/";
-cred = credentials.Certificate("C:/Users/Administrator/Desktop/올리브앤와인/가상화폐 자산관리 프로젝트/중요파일/tuning-9b90e-firebase-adminsdk-cakmk-1015e5df95.json")
+cred = credentials.Certificate("/home/ubuntu/TuningFlask/tuning-9b90e-firebase-adminsdk-cakmk-1015e5df95.json")
 firebase_admin.initialize_app(cred, {
   'projectId': 'tuning-9b90e',
 })
-
-
 
 
 @app1.route('/get', methods = ['GET', 'POST'])
@@ -91,7 +37,7 @@ def b():
     get_currencies()
     db = firestore.client()
 
-    doc_ref = db.collection(u'cryptoData').document(u'zzz')
+    doc_ref = db.collection(u'cryptoData').document(u'tuning'+puid)
     doc_ref.set({
         u'currencies' : currencies,
         u'amount' : strs,

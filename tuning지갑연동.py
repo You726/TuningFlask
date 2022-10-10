e46b5a4e5ae0ccb5155b4551fec4b12982ecff61
You726 <cksdid726@naver.com>
2022-10-10 14:54:03 +0900

    Update tuning지갑연동.py

--- tuning지갑연동.py
+++ tuning지갑연동.py
@@ -68,10 +68,9 @@ def get_currencies():
 
     for i in balances:
         amount = 0;
-        name = "KRW-"+i['currency']
         if i['currency'] != 'KRW' and i['currency'] != 'APENFT' and i['currency'] != 'XYM':
             # print(name)
-            amount = pyupbit.get_current_price(name) * upbit.get_balance(i['currency'])
+            amount = pyupbit.get_current_price("KRW-"+i['currency']) * upbit.get_balance(i['currency'])
             # amount = upbit.get_amount(i['currency'])
             if(amount >= 1):
                 currencies.append(i['currency'])

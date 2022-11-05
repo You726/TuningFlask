8fdfc02764d33896e8dcf56c825a3766de2a3842
You726 <cksdid726@naver.com>
2022-11-05 13:00:33 +0900

    Update tuning지갑연동.py

--- tuning지갑연동.py
+++ tuning지갑연동.py
@@ -108,7 +108,7 @@ def buy_market(ticker, price):
     upbit.buy_market_order("KRW-"+ticker, (price*0.9995)-1000)
 
 def sell_market(ticker, price):
-    volume = price/pyupbit.get_current_price("KRW-"+ticker)*0.9995
+    volume = (price/pyupbit.get_current_price("KRW-"+ticker)*0.9995)-1000
     print(pyupbit.get_current_price("KRW-"+ticker))
     print(volume)
     upbit.sell_market_order("KRW-"+ticker, volume)
